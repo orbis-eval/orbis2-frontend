9a995967cccceec5bf2bd30620c57e9a08ed29b9
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2022-11-30 19:55:01 +0100

    #1: udemy course - monster - health reducing working

--- pages/monster.vue
+++ pages/monster.vue
@@ -3,17 +3,17 @@
     <section id="monster" class="container">
       <h2>Monster Health</h2>
       <div class="healthbar">
-        <div class="healthbar__value"></div>
+        <div class="healthbar__value" :style="{width: monsterHealth+'%'}"></div>
       </div>
     </section>
     <section id="player" class="container">
       <h2>Your Health</h2>
       <div class="healthbar">
-        <div class="healthbar__value"></div>
+        <div class="healthbar__value" :style="{width: playerHealth+'%'}"></div>
       </div>
     </section>
     <section id="controls">
-      <button>ATTACK</button>
+      <button @click="attackMonster">ATTACK</button>
       <button>SPECIAL ATTACK</button>
       <button>HEAL</button>
       <button>SURRENDER</button>
@@ -27,4 +27,23 @@
 
 <script setup>
 import "@/assets/css/monster.css";
+
+const playerHealth = ref(100)
+const monsterHealth = ref(100)
+
+const attackMonster = () => {
+  monsterHealth.value -= randomAttackValue(12, 5)
+  attackPlayer()
+  console.log(playerHealth.value)
+  console.log(monsterHealth.value)
+}
+
+const attackPlayer = () => {
+  playerHealth.value -= randomAttackValue(18, 8)
+}
+
+let randomAttackValue = (max, min) => {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
 </script>
