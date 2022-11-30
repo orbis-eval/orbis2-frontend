75e6fde6f8a00f51166e7b312e5a75a86d94a9f8
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2022-11-30 11:43:18 +0100

    added tailwind classes to body/html tag, added template to app.vue

--- app.vue
+++ app.vue
@@ -89,7 +89,7 @@
     </header>
     <main>
       <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
+        here comes my content
         <div class="px-4 py-6 sm:px-0">
           <div class="h-96 rounded-lg border-4 border-dashed border-gray-200" />
         </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      meta: [
+        // <meta name="viewport" content="width=device-width, initial-scale=1">
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      bodyAttrs: {
+        class: 'h-full' // for tailwind template
+      },
+      htmlAttrs: {
+        class: 'h-full bg-gray-100' // for tailwind template
+      }
+    }
+  }
 })
