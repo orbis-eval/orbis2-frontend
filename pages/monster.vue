a5c6d3a2e9affdf35b4cfe2f0c0d457c97a8edaf
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2022-12-01 10:05:27 +0100

    #1: udemy course - monster - added watcher to detect winner, restart-game, handling computed properties properly, logic for special attack

--- pages/monster.vue
+++ pages/monster.vue
@@ -12,11 +12,18 @@
         <div class="healthbar__value" :style="playerStyleBar"></div>
       </div>
     </section>
+    <section class="container" v-if="winner">
+      <h2>Game Over</h2>
+      <h3 v-if="winner === 'monster'">You lost!</h3>
+      <h3 v-else-if="winner === 'player'">You win!</h3>
+      <h3 v-else>draw</h3>
+    </section>
     <section id="controls">
       <button @click="attackMonster">ATTACK</button>
-      <button>SPECIAL ATTACK</button>
-      <button>HEAL</button>
+      <button :disabled="canUseSpecialAttack" @click="specialAttackMonster">SPECIAL ATTACK</button>
+      <button @click="healPlayer">HEAL</button>
       <button>SURRENDER</button>
+      <button @click="startGame">Restart Game</button>
     </section>
     <section id="log" class="container">
       <h2>Battle Log</h2>
@@ -30,35 +37,85 @@ import "@/assets/css/monster.css";
 
 const playerHealth = ref(100)
 const monsterHealth = ref(100)
+const rounds = ref(0)
+const winner = ref(null)
 
 const attackMonster = () => {
-  monsterHealth.value -= randomAttackValue(12, 5)
+  monsterHealth.value -= randomValue(12, 5)
   attackPlayer()
-  console.log(playerHealth.value)
-  console.log(monsterHealth.value)
+  rounds.value++
+}
+
+const specialAttackMonster = () => {
+  rounds.value++
+  monsterHealth.value -= randomValue(25, 10)
+  attackPlayer()
 }
 
 const attackPlayer = () => {
-  playerHealth.value -= randomAttackValue(18, 8)
+  playerHealth.value -= randomValue(18, 8)
 }
 
-let randomAttackValue = (max, min) => {
+const healPlayer = () => {
+  const healValue = randomValue(20, 8)
+  if (playerHealth.value + healValue > 100 ) {
+    playerHealth.value = 100
+  }
+  else {
+    playerHealth.value += healValue
+  }
+}
+
+const randomValue = (max, min) => {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
+const startGame =  () => {
+  playerHealth.value = 100
+  monsterHealth.value = 100
+  rounds.value = 0
+  winner.value = null
+}
+
+watch(playerHealth, (newPlayerHealth, oldPlayerHealth) => {
+      if(newPlayerHealth<=0 && monsterHealth.value <=0) {
+        // a draw
+        winner.value = "draw"
+      }
+      else if(newPlayerHealth<=0) {
+        // player lost
+        winner.value = "monster"
+      }
+    }
+)
+
+watch(monsterHealth, (newMonsterHealth, oldMonsterHealth) => {
+      if(newMonsterHealth<=0 && playerHealth.value <=0) {
+        // a draw
+        winner.value = "draw"
+      }
+      else if(newMonsterHealth<=0){
+        // monster lost
+        winner.value = "player"
+      }
+    }
+)
+
 const monsterStyleBar = computed(
     () => {
-      return {
-        width: monsterHealth.value + '%'
-      }
+      return (monsterHealth.value <= 0) ? { width:'0%' } : {width: monsterHealth.value + '%'}
     }
 )
 
 const playerStyleBar = computed(
     () => {
-      return {
-        width: playerHealth.value + '%'
-      }
+      return (playerHealth.value <= 0) ? { width:'0%' } : {width: playerHealth.value + '%'}
+    }
+)
+
+const canUseSpecialAttack = computed(
+    () => {
+      return rounds.value%3!==0 || rounds.value==0
     }
 )
 
