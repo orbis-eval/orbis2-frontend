a8685fc5e8b7e5552ee9c4fd8c00178f8d97a0fd
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2022-12-13 11:06:15 +0100

    #1: basic grid layout start

---
+++ pages/grid-layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-8 bg-amber-300">
+    <div class="grid grid-cols-3 gap-2">
+      <header class="col-span-full tile bg-teal-500 p-4">
+        <!-- Header -->
+        Header
+      </header>
+      <nav class="tile bg-teal-500 p-4">
+        <!-- Nav Bar -->
+        Nav-Bar
+      </nav>
+      <main class="tile bg-teal-500 p-4 h-48">
+        <!-- Main Content -->
+        Main-Content
+      </main>
+      <aside class="tile bg-teal-500 p-4">
+        <!-- Sidebar -->
+        Side-Bar
+      </aside>
+      <footer class="col-span-full tile bg-teal-500 p-4">
+        <!-- Footer -->
+        Footer
+      </footer>
+    </div>
+  </div>
+</template>
