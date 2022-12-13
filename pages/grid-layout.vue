34535eea0b2b4d8d59e01272f42e7d25f5742cca
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2022-12-13 14:49:15 +0100

    #1: added tag-icon from heroicons

--- pages/grid-layout.vue
+++ pages/grid-layout.vue
@@ -28,7 +28,7 @@
             <!-- Labels -->
             <button
                 class="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
-              <LabelIcon/>
+              <TagIcon class="w-6 h-6"/>
               <p class="text-base leading-4">Labels</p>
             </button>
           </div>
@@ -51,7 +51,5 @@
 </template>
 
 <script setup>
-import {Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems} from '@headlessui/vue'
-import {UserIcon, CircleStackIcon} from '@heroicons/vue/24/outline'
-
+import {UserIcon, CircleStackIcon, TagIcon} from '@heroicons/vue/24/outline'
 </script>
