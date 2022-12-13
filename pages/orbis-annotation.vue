6c4ff63e659efb9cf26d1231eeef73eecf845126
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2022-12-13 11:44:28 +0100

    #1: nav-bar added, colors adjustment

---
+++ pages/orbis-annotation.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div class="container mx-auto 2xl border-solid border-2 px-4">
+
+    <h2 class="text-4xl font-bold text-center py-8">
+      Orbis Annotation View
+    </h2>
+
+    <div class="flex flex-row">
+      <div class="basis-1/4 border-solid border-2">01</div>
+      <div class="basis-1/4 border-solid border-2">02</div>
+      <div class="basis-1/2 border-solid border-2">03</div>
+    </div>
+  </div>
+
+</template>
