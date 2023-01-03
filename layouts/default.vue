ca75fdccd9f01573ce52c9d88e78a1b7f370b5b0
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-03 11:23:33 +0100

    refactoring: extract basis to layout, so it could be reused

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-screen text-gray-400 bg-gray-900">
+    <div class="grid grid-cols-[1fr_7fr_2fr] grid-rows-[1fr_10fr_1fr] h-full">
+      <header class="col-span-full tile bg-gray-700 p-4 text-gray-200 rounded-t-lg">
+        <div class="flex flex-row">
+          <div class="basis-1/12 ">
+            <img src="~/assets/img/Orbis-Logo-Transparent.png" class="w-20 h-20 rounded-full border border-gray-600 bg-white"/>
+          </div>
+          <div class="basis-1/12 pt-6 ">Orbis NG</div>
+          <div class="basis-10/12 " ></div>
+        </div>
+      </header>
+      <nav class="tile p-4">
+        <!-- Menu Entries -->
+        <LeftMenu/>
+      </nav>
+      <main class="tile p-4 ">
+        <slot />
+      </main>
+      <aside class="tile p-4">
+        <!-- Sidebar -->
+        Side-Bar
+      </aside>
+      <footer class="col-span-full tile p-6 rounded-b-lg text-center bg-gray-700">
+        <!-- Footer -->
+        Orbis NG - 2023
+      </footer>
+    </div>
+  </div>
+</template>
