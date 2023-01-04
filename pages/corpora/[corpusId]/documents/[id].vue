b08e962f0ba92fe2700bf0436fc509b9a83073aa
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-04 17:27:34 +0100

    use deserialization in document view

--- pages/corpora/[corpusId]/documents/[id].vue
+++ pages/corpora/[corpusId]/documents/[id].vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>Content</h1>
-  <div>
-    {{data.content}}
+  <LoadingSpinner v-if="!content" />
+  <div v-else>
+    <h1>Content</h1>
+    <div>
+      {{content}}
+    </div>
   </div>
 </template>
 
 <script setup>
+import {Error} from "~/lib/model/error";
+
 const {$orbisRepositoryService} = useNuxtApp()
-const { data, refresh, pending, error } = await $orbisRepositoryService.getDocument('3163909713')
+const route = useRoute()
+const content = ref(null)
+$orbisRepositoryService.getDocument(route.params.id)
+    .then(document => {
+      if (document instanceof Error) {
+        console.error(document.errorMessage);
+      } else {
+        content.value = document.content
+      }
+    })
 </script>
+
--- plugins/orbisRepositoryPlugin/orbis.repository.service.ts
+++ plugins/orbisRepositoryPlugin/orbis.repository.service.ts
@@ -1,3 +1,6 @@
+import {Document} from "~/lib/model/document";
+import {Error} from "~/lib/model/error";
+
 export class OrbisRepositoryService {
 
     private readonly orbisapibase: string
@@ -18,7 +21,16 @@ export class OrbisRepositoryService {
         return (await useAsyncData(() => $fetch(`${this.orbisapibase}getDocuments?corpus_id=${corpusId}`)))
     }
 
-    async getDocument(documentId: string) {
-        return (await useAsyncData(() => $fetch(`${this.orbisapibase}getDocument?document_id=${documentId}`)))
+    async getDocument(documentId: string): Promise<Document|Error> {
+        return fetch(`${this.orbisapibase}getDocument?document_id=${documentId}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                return new Document(data);
+            })
+            .catch(error => {
+                return new Error(error);
+            })
     }
 }
