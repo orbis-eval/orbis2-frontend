eea13f0da5dc9f02ab1a23aea77f7ef45d4a577c
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2023-01-04 16:44:20 +0100

    #10: POC annotation view with basic styled annotations and selection handler

---
+++ pages/annotationpoc.vue
@@ -0,0 +1,52 @@
+<template>
+  <div v-on:mouseup="onMouseUp" class="p-4">
+    <div class="line">
+      <span>
+      DEIN KARRIERESCHRITT BEI DEN FINANCIAL SERVICES IN ZÜRICH
+      </span>
+    </div>
+    <div class="line">
+      <span>&nbsp;Du schätzt ein modernes und kollegiales</span>
+      <span class="annotation">&nbsp;Arbeitsumfeld,</span>
+      <span>&nbsp;anspruchsvolle</span>
+      <span class="annotation">&nbsp;Tätigkeiten</span>
+      <span>&nbsp;und Kundennähe? Du bist es gewohnt, selbstständig zu arbeiten, bist aber gleichzeitig ein hilfsbereiter und </span>
+      <span class="annotation">
+         flexibler
+        <span class="annotation">&nbsp;Teamplayer? </span>
+      </span>
+      <span>&nbsp;Dann sollten wir uns kennenlernen.</span>
+    </div>
+    <div class="line">DAS BEWEGST DU</div>
+    <div class="line">- Du bist verantwortlich für einen reibungslosen Support des Office Assistenz Teams</div>
+    <div class="line">- Du organisierst Sitzungen, Konferenzen und weitere anspruchsvolle Anlässe</div>
+  </div>
+  <div class="p-4">
+    selected content: {{ selectedText }} ({{ selectionStart }}/{{ selectionEnd }})
+  </div>
+</template>
+
+<script setup>
+
+const selectedText = ref('')
+const selectionStart = ref(null)
+const selectionEnd = ref(null)
+
+/**
+ * mouse up event handler on
+ */
+const onMouseUp = () => {
+
+  // get the selection from the window
+  const selection = window.getSelection();
+
+  // get the word string
+  selectedText.value = selection.toString();
+
+  // get start/end index of selected text
+  const range = selection.getRangeAt(0).cloneRange();
+  selectionStart.value = range.startOffset
+  selectionEnd.value = range.endOffset
+}
+
+</script>
