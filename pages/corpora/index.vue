b6cf551cfb4d5a20a622e2c72cfce0f177f38de3
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-06 17:18:05 +0100

    change corpora overview: use newly parser method and add loading spinner

--- pages/corpora/index.vue
+++ pages/corpora/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <LoadingSpinner v-if="!corpora" />
+  <div v-else>
     <ul>
-      <li v-for="corpus in data" :key="corpus._id">
+      <li v-for="corpus in corpora" :key="corpus._id">
         <NuxtLink :to="`corpora/${corpus._id}/documents`">
           {{ corpus._id }}
         </NuxtLink>
@@ -12,5 +13,15 @@
 
 <script setup lang="ts">
 const {$orbisRepositoryService} = useNuxtApp()
-const { data, refresh, pending, error } = await $orbisRepositoryService.getCorpora()
+const route = useRoute()
+const corpora = ref(null)
+// const { data, refresh, pending, error } = await $orbisRepositoryService.getCorpora()
+$orbisRepositoryService.getCorpora()
+    .then(result => {
+      if (Array.isArray(result)) {
+        corpora.value = result;
+      } else {
+        console.error(result.errorMessage);
+      }
+    })
 </script>
