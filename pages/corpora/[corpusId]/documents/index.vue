7ca3ba144f6ae908e82a9e128cfa664870dca491
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-09 14:23:45 +0100

    refactoring: remove outcommented code

--- pages/corpora/[corpusId]/documents/index.vue
+++ pages/corpora/[corpusId]/documents/index.vue
@@ -24,23 +24,6 @@
           </tr>
           </tbody>
         </table>
-        <!--    <ul class="flex flex-wrap">-->
-        <!--      <li v-for="document in data" :key="document._id" class="p-1 object-fill overflow-hidden">-->
-        <!--        <div class="mx-auto max-w-md rounded-lg bg-white shadow">-->
-        <!--            <NuxtLink :to="`documents/${document._id}`">-->
-
-        <!--              <div class="p-4">-->
-        <!--                <h3 class="text-xl font-medium text-gray-900">-->
-        <!--                  {{ document._id }}-->
-        <!--                </h3>-->
-        <!--                <p class="text-gray-500">-->
-        <!--                  {{ document.content.substring(0, 150) }}...-->
-        <!--                </p>-->
-        <!--              </div>-->
-        <!--            </NuxtLink>-->
-        <!--        </div>-->
-        <!--      </li>-->
-        <!--    </ul>-->
       </div>
   </NuxtLayout>
 </template>
