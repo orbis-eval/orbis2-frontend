feab5dc4ccdc8bc0193b2acb6e214ff9794534e5
Norman Suesstrunk <norman.suesstrunk@fhgr.ch>
2023-01-11 17:36:59 +0100

    #10: POC - adding gap-annotation when building the tree

--- lib/model/nestedset/nestedSet.ts
+++ lib/model/nestedset/nestedSet.ts
@@ -5,6 +5,21 @@ import {Annotator} from "~/lib/model/annotator";
 
 export class NestedSet {
 
+    public static readonly GAP_ANNOTATION_KEY = "GAP_ANNOTATION_KEY";
+    public static readonly GAP_ANNOTATOR_NAME = "NESTEDSET_ANNOTATOR";
+    public static readonly GAP_ANNOTATION_TYPE_NAME = "GAP_ANNOTATION";
+
+    public static GAP_ANNOTATION_TYPE = new AnnotationType({
+        name: this.GAP_ANNOTATION_TYPE_NAME,
+        _id: 1000
+    });
+
+    public static NESTED_SET_ANNOTATOR = new Annotator({
+        name: this.GAP_ANNOTATOR_NAME,
+        roles: [],
+        _id: 1001
+    });
+
     static toTree(
         annotations: Annotation[],
         documentString: string,
@@ -13,7 +28,7 @@ export class NestedSet {
         runId: number,
         documentId: number,
         timestamp: Date,
-        id: number):NestedSetNode {
+        id: number): NestedSetNode {
 
         // sort annotations start-index and then smaller end-indexes first if start-index is the same
         annotations.sort((a: Annotation, b: Annotation) => {
@@ -40,18 +55,48 @@ export class NestedSet {
         );
 
         let previousNode = rootNode;
+        let gapStart = 0;
+
         // iterate all annotations
         for (let annotation of annotations) {
             let currentNode = new NestedSetNode(annotation);
             let parentNode = this.getParent(previousNode, currentNode);
+
+            // determine if we have a gap to the next annotation.
+            // if yes, generate an annotation of type "GAP"
+            if (parentNode === rootNode && currentNode.start_indices[0] > gapStart) {
+                let surfaceForm = documentString.substring(gapStart, currentNode.start_indices[0])
+                    .trim(); // trim whitespaces
+                // only add gap-annotation if it is not an empty string after trimming
+                if (surfaceForm) {
+                    let gapAnnotation = new NestedSetNode({
+                        key: this.GAP_ANNOTATION_KEY,
+                        surface_forms: [surfaceForm],
+                        start_indices: [gapStart],
+                        end_indices: [currentNode.start_indices[0]],
+                        annotation_type: this.GAP_ANNOTATION_TYPE,
+                        annotator: this.NESTED_SET_ANNOTATOR,
+                        run_id: annotation.run_id,
+                        document_id: annotation.document_id,
+                        metadata: [],
+                        timestamp: annotation.timestamp,
+                        _id: 2000
+                    });
+                    // add the gap-annotation to the root-node
+                    rootNode.append(gapAnnotation);
+                }
+                gapStart = currentNode.start_indices[0];
+            } else {
+                gapStart = currentNode.end_indices[0];
+            }
+            // at the node to its parent
             if (parentNode) {
                 parentNode.append(currentNode);
             } else {
-                // TODO: handle exception
+                // TODO: handle exception, this should not occur with proper annotation data
             }
             previousNode = currentNode;
         }
-
         return rootNode;
     }
 
--- tests/unit/lib/model/nestedset/nestedset.test.ts
+++ tests/unit/lib/model/nestedset/nestedset.test.ts
@@ -5,19 +5,8 @@ import {Annotator} from "~/lib/model/annotator";
 import {NestedSet} from "~/lib/model/nestedset/nestedSet";
 
 describe('Nested Set', () => {
-    test('test nested set', () => {
+    test('test annotations to tree - large example', () => {
         let documentContent = 'AA BB CC DD EE FF GG HH II';
-
-        let annotationType: AnnotationType = new AnnotationType({
-            name: "A Type",
-            _id: 1
-        });
-
-        let annotator: Annotator = new Annotator({
-            name: "test annotator",
-            roles: [],
-            _id: 1
-        });
 
         let mockAnnotations: Annotation[] = [
             mockAnnotation(
@@ -88,28 +77,73 @@ describe('Nested Set', () => {
             new Date(),
             1);
 
-        // check if tree was correctly constructed
-        expect(rootNode.children.length)
-            .toEqual(4);
+        expect(rootNode.children.length).toEqual(6);
         expect(rootNode.children[0]._id).toEqual(1);
-        expect(rootNode.children[1]._id).toEqual(2);
-        expect(rootNode.children[2]._id).toEqual(4);
-        expect(rootNode.children[3]._id).toEqual(7);
-        expect(rootNode.children[0].children.length)
-            .toEqual(0);
-        expect(rootNode.children[1].children.length)
-            .toEqual(1);
-        expect(rootNode.children[1].children[0]._id)
-            .toEqual(3);
-        expect(rootNode.children[2].children.length)
-            .toEqual(1);
-        expect(rootNode.children[2].children[0]._id)
-            .toEqual(5);
-        expect(rootNode.children[3].children.length)
-            .toEqual(1);
-        expect(rootNode.children[3].children[0]._id)
-            .toEqual(6);
+        expect(rootNode.children[2]._id).toEqual(2);
+        expect(rootNode.children[4]._id).toEqual(4);
+        expect(rootNode.children[5]._id).toEqual(7);
+
+        expect(rootNode.children[0].children.length).toEqual(0);
+
+        expect(rootNode.children[1].annotation_type.name).toEqual(NestedSet.GAP_ANNOTATION_TYPE.name);
+
+        expect(rootNode.children[2].children.length).toEqual(1);
+        expect(rootNode.children[2].children[0]._id).toEqual(3);
+
+        expect(rootNode.children[3].annotation_type.name).toEqual(NestedSet.GAP_ANNOTATION_TYPE.name);
+
+        expect(rootNode.children[4].children.length).toEqual(1);
+        expect(rootNode.children[4].children[0]._id).toEqual(5);
+
+        expect(rootNode.children[5].children.length).toEqual(1);
+        expect(rootNode.children[5].children[0]._id).toEqual(6);
     });
+
+    test('test annotations to tree - simple example', () => {
+        let documentContent = 'AABBCC';
+
+        let mockAnnotations: Annotation[] = [
+            mockAnnotation(
+                'AA',
+                0,
+                2,
+                1,
+                annotationType,
+                annotator,
+            ),
+            mockAnnotation(
+                'CC',
+                4,
+                6,
+                2,
+                annotationType,
+                annotator,
+            )
+        ];
+
+        let rootNode = NestedSet.toTree(
+            mockAnnotations,
+            documentContent,
+            annotationType,
+            annotator,
+            1,
+            1,
+            new Date(),
+            1);
+
+        expect(rootNode.children.length).toEqual(3);
+    });
+});
+
+let annotationType: AnnotationType = new AnnotationType({
+    name: "A Type",
+    _id: 1
+});
+
+let annotator: Annotator = new Annotator({
+    name: "test annotator",
+    roles: [],
+    _id: 1
 });
 
 function mockAnnotation(
