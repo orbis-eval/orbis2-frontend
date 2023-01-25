ebe96e27a925a45bf653cc0ac9684d4316e43e62
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-25 11:01:31 +0100

    add test for add new annotation and no undoing possible scenario, refactor

--- tests/unit/stores/annotationStore.test.ts
+++ tests/unit/stores/annotationStore.test.ts
@@ -5,51 +5,71 @@ import {AnnotationType} from "~/lib/model/annotationType";
 import {Annotator} from "~/lib/model/annotator";
 import {createPinia, setActivePinia} from "pinia";
 
+let mockedAnnotation: Annotation;
+
 beforeEach(() => {
     // necessary, otherwise the global store is not available
     setActivePinia(createPinia());
+    mockedAnnotation = new Annotation({
+        key: 'my-key',
+        surface_forms: ['surfaceForm'],
+        start_indices: [0],
+        end_indices: [12],
+        annotation_type: new AnnotationType({
+            name: 'type1',
+            _id: 1
+        }),
+        annotator: new Annotator({
+            name: 'name',
+            roles: [],
+            _id: 2
+        }),
+        run_id: 1,
+        document_id: 1,
+        metadata: [],
+        timestamp: new Date(),
+        _id: 3
+    });
 })
-
-const ANNOTATION = new Annotation({
-    key: 'my-key',
-    surface_forms: ['surfaceForm'],
-    start_indices: [0],
-    end_indices: [12],
-    annotation_type: new AnnotationType({
-        name: 'type1',
-        _id: 1
-    }),
-    annotator: new Annotator({
-        name: 'name',
-        roles: [],
-        _id: 2
-    }),
-    run_id: 1,
-    document_id: 1,
-    metadata: [],
-    timestamp: new Date(),
-    _id: 3
-});
 
 describe('AnnotationStore.addAnnotation()', () => {
     test('add annotation to annotations list, list contains one element afterwards', () => {
         const annotationStore = useAnnotationStore();
 
         expect(annotationStore.annotations.length).toEqual(0);
-        annotationStore.addAnnotation(ANNOTATION);
+        annotationStore.addAnnotation(mockedAnnotation);
         expect(annotationStore.annotations.length).toEqual(1);
     });
+
+    test('simulate undoing and adding a new annotation, after adding new annotation, ' +
+        'redoing is no longer possible', () => {
+        const annotationStore = useAnnotationStore();
+        let annotation2 = new Annotation(mockedAnnotation);
+        annotation2.key = 'my-key2'
+        let annotation3 = new Annotation(mockedAnnotation);
+        annotation3.key = 'my-key3'
+
+        annotationStore.addAnnotation(mockedAnnotation);
+        annotationStore.addAnnotation(annotation2);
+        annotationStore.undoAnnotation(() => {});
+        annotationStore.addAnnotation(annotation3)
+
+        expect(annotationStore.undoneAnnotations.length).toEqual(0)
+        expect(annotationStore.annotations.length).toEqual(2);
+        expect(annotationStore.annotations[0].key).toEqual('my-key');
+        expect(annotationStore.annotations[1].key).toEqual('my-key3');
+    })
 });
 
 describe('AnnotationStore.popAnnotation()', () => {
     test('pop last annotation from annotations list, list is empty afterwards', () => {
         const annotationStore = useAnnotationStore();
 
-
-        annotationStore.addAnnotation(ANNOTATION);
+        annotationStore.addAnnotation(mockedAnnotation);
         let annotation = annotationStore.popAnnotation();
+
         expect(annotationStore.annotations.length).toEqual(0);
-        expect(annotation?.key).toEqual(ANNOTATION.key);
+        expect(annotation?.key).toEqual('my-key');
     });
 
     test('annotations list is empty, pop last element does not throw any error nor change the state, ' +
@@ -57,6 +77,7 @@ describe('AnnotationStore.popAnnotation()', () => {
         const annotationStore = useAnnotationStore();
 
         let annotation = annotationStore.popAnnotation();
+
         expect(annotationStore.annotations.length).toEqual(0);
         expect(annotation).toBeUndefined();
     })
@@ -67,16 +88,21 @@ describe('AnnotationStore.undoAnnotation()', () => {
         () => {
         const annotationStore = useAnnotationStore();
         let callbackCounter = 0;
-
-        let annotation2 = ANNOTATION;
+        let annotation2 = new Annotation(mockedAnnotation);
         annotation2.key = 'my-key2';
-        annotationStore.addAnnotation(ANNOTATION);
+        let annotation3 = new Annotation(mockedAnnotation);
+        annotation3.key = 'my-key3';
+
+        annotationStore.addAnnotation(mockedAnnotation);
         annotationStore.addAnnotation(annotation2);
+        annotationStore.addAnnotation(annotation3);
         annotationStore.undoAnnotation(() => { callbackCounter++; });
-        expect(annotationStore.annotations.length).toEqual(1);
+
+        expect(annotationStore.annotations.length).toEqual(2);
         expect(annotationStore.undoneAnnotations.length).toEqual(1);
-        expect(annotationStore.annotations[0].key).toEqual(ANNOTATION.key);
-        expect(annotationStore.undoneAnnotations[0].key).toEqual(annotation2.key);
+        expect(annotationStore.annotations[0].key).toEqual('my-key');
+        expect(annotationStore.annotations[1].key).toEqual('my-key2');
+        expect(annotationStore.undoneAnnotations[0].key).toEqual('my-key3');
         expect(callbackCounter).toEqual(1);
     });
 
@@ -97,17 +123,23 @@ describe('AnnotationStore.redoAnnotation()', () => {
         () => {
             const annotationStore = useAnnotationStore();
             let callbackCounter = 0;
-
-            let annotation2 = ANNOTATION;
+            let annotation2 = new Annotation(mockedAnnotation);
             annotation2.key = 'my-key2';
-            annotationStore.addAnnotation(ANNOTATION);
+            let annotation3 = new Annotation(mockedAnnotation);
+            annotation3.key = 'my-key3';
+
+            annotationStore.addAnnotation(mockedAnnotation);
             annotationStore.addAnnotation(annotation2);
+            annotationStore.addAnnotation(annotation3);
+            annotationStore.undoAnnotation(() => {});
             annotationStore.undoAnnotation(() => {});
             annotationStore.redoAnnotation(() => { callbackCounter++; })
+
             expect(annotationStore.annotations.length).toEqual(2);
-            expect(annotationStore.undoneAnnotations.length).toEqual(0);
-            expect(annotationStore.annotations[0].key).toEqual(ANNOTATION.key);
-            expect(annotationStore.annotations[1].key).toEqual(annotation2.key);
+            expect(annotationStore.undoneAnnotations.length).toEqual(1);
+            expect(annotationStore.annotations[0].key).toEqual('my-key');
+            expect(annotationStore.annotations[1].key).toEqual('my-key2');
+            expect(annotationStore.undoneAnnotations[0].key).toEqual('my-key3');
             expect(callbackCounter).toEqual(1);
         });
 
