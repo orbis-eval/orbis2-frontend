6f861a2a64b316a2e743d18d2564691ed6b90d44
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-27 14:06:20 +0100

    generalize item styling in leftMenu

--- components/leftMenu.vue
+++ components/leftMenu.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col justify-start items-center w-full">
     <div id="menu1" class="flex justify-start  flex-col w-full md:w-auto items-start">
       <!-- Runs -->
-      <div v-if="runs" class="flex justify-start items-center space-x-6  text-gray-400 rounded px-3 py-2 w-full md:w-52;">
+      <div v-if="runs" class="menu-item">
         <OhVueIcon name="la-rocket-solid" class="menu-icon"/>
         <select v-model="currentSelection" >
           <option v-for="run in runs" :value="run">
