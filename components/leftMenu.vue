1acb081edccd3b78f5b71893daba0566949d2b7b
Andreas Fraefel <andreas.fraefel@fhgr.ch>
2023-01-27 14:04:09 +0100

    change styling of selection in leftMenu

--- components/leftMenu.vue
+++ components/leftMenu.vue
@@ -2,14 +2,14 @@
   <div class="flex flex-col justify-start items-center w-full">
     <div id="menu1" class="flex justify-start  flex-col w-full md:w-auto items-start">
       <!-- Runs -->
-      <button v-if="runs" class="menu-button">
+      <div v-if="runs" class="flex justify-start items-center space-x-6  text-gray-400 rounded px-3 py-2 w-full md:w-52;">
         <OhVueIcon name="la-rocket-solid" class="menu-icon"/>
         <select v-model="currentSelection" >
           <option v-for="run in runs" :value="run">
             {{ run._id }}
           </option>
         </select>
-      </button>
+      </div>
       <!-- Documents -->
       <button class="menu-button">
         <OhVueIcon name="hi-database" class="menu-icon"/>
