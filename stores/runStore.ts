938aa2248783b4ad82343763fe9471d4ac7aaf09
roger <roger.waldvogel@fhgr.ch>
2023-07-13 17:25:31 +0200

    Initial implementation for the run store.

---
+++ stores/runStore.ts
@@ -0,0 +1,36 @@
+import {defineStore} from "pinia";
+import {Run} from "~/lib/model/run";
+
+import {OrbisApiService} from "~/lib/orbisApi/orbisApiService";
+
+export const useRunStore = defineStore('run', {
+    state: () => {
+        return {
+            corpusId: -1,
+            runs: [] as Run[],
+            selectedRun: {} as Run
+        };
+    },
+    actions: {
+        $reset() {
+            this.corpusId = -1;
+            this.runs = [] as Run[];
+            this.selectedRun = {} as Run;
+        },
+        changeSelectedRun(run: Run) {
+            console.log("changeSelectedRun")
+            console.log(run)
+            this.selectedRun = run;
+        },
+        async loadRuns(corpusId: number, orbisApiService: OrbisApiService) {
+            if (this.corpusId !== corpusId) {
+                const runs = await orbisApiService.getRuns(corpusId);
+                if (Array.isArray(runs) && runs.length > 0) {
+                    this.corpusId = corpusId;
+                    this.runs = runs;
+                    this.selectedRun = this.runs[0];
+                }
+            }
+        }
+    }
+});
