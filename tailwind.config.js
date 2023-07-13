696b64959eb36893eb6350b96cf793de1a3d566c
roger <roger.waldvogel@fhgr.ch>
2023-07-13 17:25:48 +0200

    Defined orbis theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                orbisDefaultTheme: {
+                    "primary": "#C084FC",
+                    "secondary": "#f6d860",
+                    "accent": "#37cdbe",
+                    "neutral": "#111827",
+                    "base-100": "#374151",
+                    "info": "#3abff8",
+                    "success": "#36d399",
+                    "warning": "#fbbd23",
+                    "error": "#f87272",
+                },
+            }
+        ],
+    }
 };
