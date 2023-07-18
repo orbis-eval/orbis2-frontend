fe830da0f9d3f6ea0f1ec5c4d5cd92e784391f75
Andreas Murk <andreas.murk@fhgr.ch>
2023-07-18 11:18:18 +0200

    fix(frontend): toggle and close once selected

--- components/runDropdown.vue
+++ components/runDropdown.vue
@@ -1,23 +1,22 @@
 <template>
-<div v-if="runs" class="bg-neutral flex border border-gray-500 rounded-xl p-2 mt-20">
-<div class="w-4/5">
-  <details class="dropdown w-full" @click="toggleDropdown">
-    <summary class="m-1 btn bg-gray-100 text-black w-full">
-      {{ selectedRun && selectedRun.name ? selectedRun.name : "Please Select Your Run" }}
-      <OhVueIcon name="md-keyboardarrowdown"/>
-    </summary>
-      <ul v-show="isOpen" class="text-black p-2 shadow menu dropdown-content z-[1] bg-gray-100 rounded-md w-full">
-      <li v-for="run in runs" :key="runs._id">
-        <!--  TODO: not be able to toggle -->
-        <NuxtLink class="link" @click="selectedRunChanged(run)">
-          {{ run.name }}
-        </NuxtLink>
-      </li>
-    </ul>
-  </details>
-</div>
-<button class="m-1 btn w-1/5">Runs</button>
-</div>
+  <div v-if="runs" class="bg-neutral flex border border-gray-500 rounded-xl p-2 mt-20">
+    <div class="w-4/5">
+      <details class="dropdown w-full">
+        <summary class="m-1 btn bg-gray-100 text-black hover:text-white w-full">
+          {{ selectedRun && selectedRun.name ? selectedRun.name : "Please Select Your Run" }}
+          <OhVueIcon name="md-keyboardarrowdown"/>
+        </summary>
+        <ul class="text-black hover:text-black p-2 shadow menu dropdown-content z-[1] bg-gray-100 rounded-md w-full">
+          <li v-for="run in runs" :key="runs._id">
+            <NuxtLink class="link" @click="selectedRunChanged(run)">
+              {{ run.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </details>
+    </div>
+    <button class="m-1 btn w-1/5">Runs</button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -45,17 +44,17 @@ await runStore.loadRuns(props.currentCorpus?._id || 0, props.orbisApiService);
 const runs = ref(runStore.runs);
 const {selectedRun} = storeToRefs(runStore);
 
-const isOpen = ref(false);
-
 function selectedRunChanged(run: any) {
+  console.log("clicked");
   if (run?._id) {
     runStore.changeSelectedRun(run);
-    isOpen.value = false;
+
+    // close dropdown once clicked
+    const dropdown = document.querySelector(".dropdown");
+    if (dropdown) {
+      dropdown.removeAttribute("open");
+    }
   }
 }
 
-function toggleDropdown() {
-  // toggle between open and closed
-  isOpen.value = !isOpen.value;
-}
 </script>
