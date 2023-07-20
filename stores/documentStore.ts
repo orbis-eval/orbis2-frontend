66b07541646ec0bd20690f17b22b5c9d7012f2f9
Andreas Murk <andreas.murk@fhgr.ch>
2023-07-20 16:58:19 +0200

    feat(frontend): introduce new documentStore

---
+++ stores/documentStore.ts
@@ -0,0 +1,68 @@
+import {defineStore} from "pinia";
+import {OrbisApiService} from "~/lib/orbisApi/orbisApiService";
+import {Document} from "~/lib/model/document";
+
+export const useDocumentStore = defineStore('document', {
+    state: () => {
+        return {
+            documents: [] as Document[],
+            nrOfDocuments: 1,
+            nrOfPages: 1,
+            currentSelectedDocPage: 1
+        };
+    },
+    actions: {
+        $reset() {
+            this.documents = [] as Document[];
+            this.currentSelectedDocPage = 1;
+        },
+
+        async getDocuments(corpusId: number, orbisApiService: OrbisApiService, pageSize: number | undefined = undefined, skip: number = 0) {
+            if (corpusId === undefined) {
+                console.error("No corpusId provided!");
+                return;
+            }
+
+            try {
+                const documents = await orbisApiService.getDocuments(corpusId, pageSize, skip);
+
+                if (Array.isArray(documents)) {
+                    this.documents = documents;
+                } else {
+                    return new Error("The response of the documents seems invalid.");
+                }
+
+            } catch (error) {
+                return new Error("An error occurred while fetching documents.");
+            }
+        },
+
+        // TODO: should the number of documents come from either the current corpus or the run?
+        async getNumberOfDocuments(corpusId: number, orbisApiService: OrbisApiService, pageSize: number | undefined = undefined, skip: number = 0) {
+            if (corpusId === undefined) {
+                console.error("No corpusId provided!");
+                return;
+            }
+
+            try {
+                const response = await orbisApiService.getNumberOfDocuments(corpusId, pageSize, skip);
+
+                if (response instanceof Error) {
+                    return response;
+                }
+
+                // Convert the response to a primitive number if it's of type Number
+                const nrOfdocuments = response instanceof Number ? Number(response) : response;
+
+                if (typeof nrOfdocuments === 'number') {
+                    this.nrOfDocuments = nrOfdocuments;
+                } else {
+                    return new Error("The response of the nOfdocuments seems invalid.");
+                }
+
+            } catch (error) {
+                return new Error("An error occurred while fetching documents.");
+            }
+        },
+    }
+});
