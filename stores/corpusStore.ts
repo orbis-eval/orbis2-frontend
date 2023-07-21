3f8f696877ad93a5763837fbe042490f4d17c1f6
Andreas Murk <andreas.murk@fhgr.ch>
2023-07-21 14:33:54 +0200

    feat(frontend): add loading corpora in corpusStore

--- stores/corpusStore.ts
+++ stores/corpusStore.ts
@@ -1,18 +1,32 @@
 import {defineStore} from "pinia";
 import {OrbisApiService} from "~/lib/orbisApi/orbisApiService";
 import {Corpus} from "~/lib/model/corpus";
+import {Error} from "~/lib/model/error";
 
 export const useCorpusStore = defineStore("corpus", {
     state: () => {
         return {
+            corpora: [] as Corpus[],
             corpus: {} as Corpus,
         };
     },
     actions: {
         $reset: () => {
             return {
+                corpora: [] as Corpus[],
                 corpus: {} as Corpus,
             };
+        },
+        async loadCorpora(orbisApiService: OrbisApiService) {
+            try {
+                const corpora = await orbisApiService.getCorpora();
+
+                if (Array.isArray(corpora) && corpora.length > 0) {
+                    this.corpora = corpora;
+                }
+            } catch (error) {
+                return new Error("An error occurred while fetching corpora.");
+            }
         },
 
         async loadCorpus(corpusId: number, orbisApiService: OrbisApiService) {
