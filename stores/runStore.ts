b981972014c466f259d94af9dde68b5a93491f89
Andreas Murk <andreas.murk@fhgr.ch>
2023-07-21 14:38:31 +0200

    feat(frontend): add error case
    - try/catch in loadRuns

--- stores/runStore.ts
+++ stores/runStore.ts
@@ -2,6 +2,7 @@ import {defineStore} from "pinia";
 import {Run} from "~/lib/model/run";
 import {OrbisApiService} from "~/lib/orbisApi/orbisApiService";
 import {Corpus} from "~/lib/model/corpus";
+import {Error} from "~/lib/model/error";
 
 export const useRunStore = defineStore('run', {
     state: () => {
@@ -29,13 +30,13 @@ export const useRunStore = defineStore('run', {
                 const response = await orbisApiService.addRun(newRun, corpus);
 
                 if (response instanceof Error) {
-                    console.error(response.message);
+                    return new Error("Something is wrong with the response");
                 } else {
                     // Reload the runs in the store after creating a new one
                     await this.loadRuns(corpus._id, orbisApiService);
                 }
             } catch (error) {
-                console.error(error);
+                return new Error("An error occurred while creating a run.");
             }
         },
 
@@ -45,11 +46,15 @@ export const useRunStore = defineStore('run', {
                 return;
             }
 
-            const runs = await orbisApiService.getRuns(corpusId);
-            if (Array.isArray(runs) && runs.length > 0) {
-                this.corpusId = corpusId;
-                this.runs = runs;
-                this.selectedRun = this.runs[0];
+            try {
+                const runs = await orbisApiService.getRuns(corpusId);
+                if (Array.isArray(runs) && runs.length > 0) {
+                    this.corpusId = corpusId;
+                    this.runs = runs;
+                    this.selectedRun = this.runs[0];
+                }
+            } catch (error) {
+                return new Error("An error occurred while fetching runs.");
             }
         }
     }
