a25b4649c70fc54b39b7000901a1fcd83818f3af
Andreas Murk <andreas.murk@fhgr.ch>
2023-09-06 13:50:19 +0200

    feat(nuxt): use .public notation for plugin

--- plugins/orbisApiPlugin.ts
+++ plugins/orbisApiPlugin.ts
@@ -2,7 +2,7 @@ import {OrbisApiService} from "~/lib/orbisApi/orbisApiService";
 
 export default defineNuxtPlugin((nuxtApp) => {
     const runtimeConfig = useRuntimeConfig();
-    const orbisApiService = new OrbisApiService(runtimeConfig.orbisApiBase);
+    const orbisApiService = new OrbisApiService(runtimeConfig.public.orbisApiBase);
     return {
         provide: {
             orbisApiService: orbisApiService
