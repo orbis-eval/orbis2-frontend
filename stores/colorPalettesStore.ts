caee8f210f66fe8d19e555887f47291775eb0b34
roger <roger.waldvogel@fhgr.ch>
2023-09-13 10:54:09 +0200

    Chg: Moved color palettes to store.

---
+++ stores/colorPalettesStore.ts
@@ -0,0 +1,27 @@
+import {defineStore} from "pinia";
+import {OrbisApiService} from "~/lib/orbisApi/orbisApiService";
+import {Error} from "~/lib/model/error";
+import {ref} from 'vue';
+import {ColorPalette} from "~/lib/model/colorpalette";
+
+export const useColorPalettesStore = defineStore('colorPalletes', () => {
+    const colorPalettes = ref([] as ColorPalette[]);
+    const currentColorPalette = ref({} as ColorPalette);
+
+    async function loadColorPalettes(orbisApiService: OrbisApiService) {
+        try {
+            const response = await orbisApiService.colorPalettes();
+
+            if (response instanceof Error) {
+                return new Error("An error occurred while fetching color palettes.");
+            }
+
+            colorPalettes.value = response;
+            currentColorPalette.value = colorPalettes.value[0];
+        } catch (error) {
+            return new Error("An error occurred while fetching color palettes.");
+        }
+    }
+
+    return {colorPalettes, currentColorPalette, loadColorPalettes};
+});
