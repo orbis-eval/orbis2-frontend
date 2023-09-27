4a12ca3af36a13e1c73a23c2c8fb77a13abdf224
roger <roger.waldvogel@fhgr.ch>
2023-09-27 17:11:09 +0200

    Fix: Removed unused code.

--- tests/unit/lib/model/nestedset/nestedSetNodeInserter.test.ts
+++ tests/unit/lib/model/nestedset/nestedSetNodeInserter.test.ts
@@ -1,9 +1,6 @@
 import {describe, expect, test} from "@jest/globals";
 import {Annotation} from "~/lib/model/annotation";
-import {AnnotationType} from "~/lib/model/annotationType";
-import {Annotator} from "~/lib/model/annotator";
 import {NestedSet} from "~/lib/model/nestedset/nestedSet";
-import {NestedSetParseError} from "~/lib/model/nestedset/nestedSetParseError";
 import {NestedSetNode} from "~/lib/model/nestedset/nestedSetNode";
 import {
     annotationType,
@@ -148,33 +145,6 @@ describe('NestedSetNodeInserter.insertAnnotationNode(...)', () => {
             expect(lineNode.children[1].start_indices[0]).toEqual(12);
             expect(lineNode.children[1].end_indices[0]).toEqual(14);
             expect(lineNode.children[1].children.length).toEqual(0);
-            //
-            //
-            // // node 'CC DD EE'
-            // let node2 = lineNode.children[2];
-            // expect(node2.start_indices[0]).toEqual(6);
-            // expect(node2.end_indices[0]).toEqual(14);
-            // expect(node2.children.length).toEqual(4);
-            //
-            // // gap node 'CC' that was calculated newly after inserting 'EE' as child of 'CC DD EE'
-            // expect(node2.children[0].surface_forms[0]).toEqual('CC ');
-            // expect(node2.children[0].start_indices[0]).toEqual(6);
-            // expect(node2.children[0].end_indices[0]).toEqual(9);
-            //
-            // // annotation 'DD'
-            // expect(node2.children[1].surface_forms[0]).toEqual('DD');
-            // expect(node2.children[1].start_indices[0]).toEqual(9);
-            // expect(node2.children[1].end_indices[0]).toEqual(11);
-            //
-            // // gap-annotation
-            // expect(node2.children[2].surface_forms[0]).toEqual(' ');
-            // expect(node2.children[2].start_indices[0]).toEqual(11);
-            // expect(node2.children[2].end_indices[0]).toEqual(12);
-            //
-            // // annotation 'EE' that was added after tree was rendered
-            // expect(node2.children[3].surface_forms[0]).toEqual('EE');
-            // expect(node2.children[3].start_indices[0]).toEqual(12);
-            // expect(node2.children[3].end_indices[0]).toEqual(14);
         }
     });
 });
