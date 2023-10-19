8fd35e0f1a99837195a0c8a11cc1b1ad688a96da
roger <roger.waldvogel@fhgr.ch>
2023-10-19 17:36:44 +0200

    Using forms, veevalidate and zod to allow only allowed run name and description.

--- components/modal/createRun.vue
+++ components/modal/createRun.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
     <h2 class="font-bold text-3xl mb-5">Create run</h2>
-    <div class="mb-4">
-      <label class="text-white block mb-1 ">Name:</label>
-      <input v-model="newRunName" class="input w-full bg-white text-black" required type="text" />
-    </div>
-    <div class="mb-4">
-      <label class="text-white block mb-1">Description:</label>
-      <input v-model="newRunDesc" class="input w-full bg-white text-black" required type="text" />
-    </div>
-    <div class="grid grid-cols-3 gap-4 mt-10">
-      <OrbisButton :onClick="createRun">Create</OrbisButton>
-      <OrbisButton :onClick="() => closeModal()">Cancel</OrbisButton>
-    </div>
+    <Form v-slot="{ errors }" :validation-schema="validationSchema" @submit="createRun">
+      <div class="mb-4">
+        <label class="text-white block mb-1 ">Name:</label>
+        <Field :class="errors.runName ? 'input-bordered input-error' : ''"
+               class="input w-full bg-white text-black mb-2"
+               name="runName"
+               type="text"/>
+        <ErrorMessage class="text-red-400" name="runName"/>
+      </div>
+      <div class="mb-4">
+        <label class="text-white block mb-1">Description:</label>
+        <Field :class="errors.runDesc ? 'input-bordered input-error' : ''"
+               class="input w-full bg-white text-black mb-2"
+               name="runDesc"
+               type="text"/>
+        <ErrorMessage class="text-red-400" name="runDesc"/>
+      </div>
+      <div class="grid grid-cols-3 gap-4 mt-10">
+        <OrbisButton :isFormButton="true">Create</OrbisButton>
+        <OrbisButton :onClick="() => closeModal()">Cancel</OrbisButton>
+      </div>
+    </Form>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {Form, Field, ErrorMessage, useForm} from 'vee-validate';
+import {toTypedSchema} from '@vee-validate/zod';
+import * as zod from 'zod';
 import {useRunStore} from "~/stores/runStore";
 import {storeToRefs} from "pinia";
 import {Run} from "~/lib/model/run";
@@ -28,17 +41,21 @@ const {closeModal} = useModal();
 const runStore = useRunStore();
 const corpusStore = useCorpusStore();
 const {corpus} = storeToRefs(corpusStore);
-const newRunName = ref("");
-const newRunDesc = ref("");
-const createRun = async () => {
+const {errors} = useForm();
+
+const validationSchema = toTypedSchema(zod.object({
+  runName: zod.string({required_error: 'Run Name is required'}),
+  runDesc: zod.string({required_error: 'Run Description is required'})
+}));
+
+const createRun = async (values: any) => {
   try {
-    const newRun = new Run({ name: newRunName.value, description: newRunDesc.value, corpus: corpus.value });
+    const newRun = new Run({name: values.runName, description: values.runDesc, corpus: corpus.value});
     await runStore.createRun(newRun, corpus.value, $orbisApiService);
   } catch (error) {
     // Todo: Show error to user
     console.error(error);
-  }
-  finally {
+  } finally {
     closeModal();
   }
 }
--- components/orbis/button.vue
+++ components/orbis/button.vue
@@ -1,14 +1,18 @@
 <template>
     <button
-        @click.prevent="clickEvent"
+        :disabled="disabled || isLoading || isFormLoading"
         :class="classesAsString"
-        :disabled="disabled || isLoading">
-        <span v-if="isLoading" class="loading loading-spinner w-4 h-4"></span>
-        <slot></slot>    
+        :type="isFormButton ? 'submit' : 'button'"
+        @click="clickEvent">
+        <span v-if="isLoading || isFormLoading" class="loading loading-spinner w-4 h-4"></span>
+        <slot></slot>
     </button>
 </template>
 
 <script setup lang="ts">
+import { useIsSubmitting} from 'vee-validate';
+
+
 const props = defineProps({
     disabled: Boolean,
     active: Boolean,
@@ -18,19 +22,34 @@ const props = defineProps({
         default: 'md'
     },
     join: Boolean, // Join button description
+    isFormButton: Boolean,
     onClick: { type: Function, required: false }
 });
 
 const isLoading = ref(false);
 
+const isFormLoading = computed(() => {
+     if (props.isFormButton) {
+        const isSubmitting = useIsSubmitting();
+        return isSubmitting.value;
+     }
+     return false;
+});
+
 /**
- * @description 
- * By clicking on the button, the loading spinner is shown and depending on the function type (async or not) 
+ * @description
+ * By clicking on the button, the loading spinner is shown and depending on the function type (async or not)
  * the function is executed. As soon as the function is finished, the loading spinner is hidden again.
- * IMPORTANT: Implement callbacks as async await functions, otherwise the loading spinner will not mirror 
+ * IMPORTANT: Implement callbacks as async await functions, otherwise the loading spinner will not mirror
  *            the duration of the function.
  */
-const clickEvent = async () => {
+const clickEvent = async (event: Event) => {
+    if(props.isFormButton) {
+        return;
+    }
+
+    event.preventDefault();
+
     try {
         isLoading.value = true;
         if (props.onClick && props.onClick.constructor.name === "AsyncFunction") {
