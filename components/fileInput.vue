05ecbe5a0ef51f3a1e0aaee76208e41546814a7a
roger <roger.waldvogel@fhgr.ch>
2023-10-23 10:46:31 +0200

    Removed pagination and using scrolling instead.

--- components/fileInput.vue
+++ components/fileInput.vue
@@ -11,13 +11,13 @@
       </div>
     </div>
     <div @drop.prevent="dropHandler" @dragover.prevent="dragOverHandler"
-         class="flex flex-col h-full justify-between border border-dashed border-gray-400">
+         class="flex flex-col h-full justify-between border border-dashed border-gray-400 max-h-96 overflow-auto">
       <div class="text-gray-600">
         <div v-if="!selectedFiles.length" class="flex h-full items-center p-4">
           <p class="w-full text-center">Or drop files here</p>
         </div>
         <div v-else class="p-1">
-          <div v-for="(file, index) in displayedFiles" class="overflow-auto flex items-center justify-between m-2 px-1">
+          <div v-for="(file, index) in selectedFiles" class="overflow-auto flex items-center justify-between m-2 px-1">
             <p id= index>{{ file.name }}</p>
             <OrbisButton :onClick="() => deleteFile(index)">
               <OhVueIcon name="md-deleteforever-outlined"/>
@@ -25,11 +25,6 @@
           </div>
         </div>
       </div>
-      <Pagination v-if="nofPages"
-                  :currentPage="currentPage"
-                  :totalPages="nofPages"
-                  @pageChanged="pageChanged"
-                  class="text-center"/>
     </div>
   </div>
 </template>
@@ -37,14 +32,11 @@
 <script setup lang="ts">
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { MdDeleteforeverOutlined } from "oh-vue-icons/icons";
-import {Corpus} from "~/lib/model/corpus";
 
 const emit = defineEmits(['fileChange']);
 
 addIcons(MdDeleteforeverOutlined);
 
-const currentPage = ref(1);
-const filesPerPage = ref(5);
 const fileInput = ref({} as HTMLInputElement);
 const selectedFiles = ref([] as File[]);
 
@@ -53,13 +45,6 @@ const props = defineProps({
     type: String,
     required: true
   }
-});
-
-const nofPages = computed(() => Math.ceil(selectedFiles.value.length / filesPerPage.value));
-const displayedFiles = computed(() => {
-  const startIndex = (currentPage.value - 1) * filesPerPage.value;
-  const endIndex = startIndex + filesPerPage.value;
-  return selectedFiles.value.slice(startIndex, endIndex);
 });
 
 function openFileInput() {
@@ -78,10 +63,6 @@ function inputChanged() {
     }
     emit('fileChange', selectedFiles.value);
   }
-}
-
-function pageChanged(nextPage: number) {
-  currentPage.value = nextPage;
 }
 
 function dragOverHandler(event: Event) {
