3189f587d570aac85a4c876aae598b32ce725756
roger <roger.waldvogel@fhgr.ch>
2023-10-23 09:59:08 +0200

    Added validation of corpus name.

--- components/modal/createCorpus.vue
+++ components/modal/createCorpus.vue
@@ -1,45 +1,55 @@
 <template>
   <div>
     <h2 class="font-bold text-3xl mb-5">Create Corpus</h2>
-    <div class="flex justify-between mb-6">
-      <div class="text-lg font-medium p-1">
-        Name of Corpus
+    <Form v-slot="{ errors }" :validation-schema="validationSchema" @submit="createCorpus">
+      <div class="mb-4">
+        <label class="text-white block mb-1 ">Name of Corpus:</label>
+        <Field :class="errors.corpusName ? 'input-bordered input-error' : ''"
+               class="input w-full bg-white text-black mb-2"
+               name="corpusName"
+               type="text"/>
+        <ErrorMessage class="text-red-400" name="corpusName"/>
       </div>
-      <input class="bg-gray-700 p-1" @input="setCorpusName"/>
-    </div>
-    <FileInput @fileChange="fileChanged"/>
-    <div class="flex gap-4 mt-5">
-      <OrbisButton id="submit" :onClick="createCorpus">Create</orbisButton>
-      <OrbisButton id="cancel" :onClick="cancel">cancel</orbisButton>
-    </div>
+      <FileInput @fileChange="fileChanged"/>
+      <div class="flex gap-4 mt-5">
+        <OrbisButton id="submit" :isFormButton="true">Create</orbisButton>
+        <OrbisButton id="cancel" :onClick="cancel">cancel</orbisButton>
+      </div>
+    </Form>
   </div>
 </template>
 
 <script lang="ts" setup>
+import {ErrorMessage, Field, Form} from "vee-validate";
+import {toTypedSchema} from "@vee-validate/zod";
+import * as zod from "zod";
 import {useCorpusStore} from "~/stores/corpusStore";
 
 const {$orbisApiService} = useNuxtApp();
 const {closeModal} = useModal();
 const corpusStore = useCorpusStore();
+const {corpora} = storeToRefs(corpusStore);
 const cancel = () => closeModal();
 
-const corpusNameToCreate = ref("");
 let chosenFiles = ref([] as File[]);
+
+const corpusNotExists = (runName: string) => {
+  return !corpora.value.some(corpus => corpus.name === runName);
+};
+
+const validationSchema = toTypedSchema(zod.object({
+  corpusName: zod.string({required_error: 'Corpus Name is required'})
+      .refine(corpusNotExists, {message: 'Corpus with this name already exists'})
+}));
 
 
 function fileChanged(newChosenFiles: File[]) {
   chosenFiles.value = newChosenFiles;
 }
 
-function setCorpusName(event: Event) {
-  if (event.target instanceof HTMLInputElement) {
-    corpusNameToCreate.value = event.target.value;
-  }
-}
-
-const createCorpus = async () => {
+const createCorpus = async (values: any) => {
   try {
-    await corpusStore.createCorpus(corpusNameToCreate.value, chosenFiles.value, $orbisApiService);
+    await corpusStore.createCorpus(values.corpusName, chosenFiles.value, $orbisApiService);
   } catch (Error) {
     // Todo: Add Error Message
     console.error(Error);
