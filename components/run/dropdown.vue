fe2e1330a72992c6057f2999d67bd63d84a0826d
roger <roger.waldvogel@fhgr.ch>
2023-10-24 17:20:05 +0200

    Removed unneeded check.

--- components/run/dropdown.vue
+++ components/run/dropdown.vue
@@ -36,7 +36,7 @@ const {selectedRun} = storeToRefs(runStore);
 const {runs} = storeToRefs(runStore);
 
 function selectedRunChanged(run: Run) {
-  if (run && run._id) {
+  if (run._id) {
     runStore.changeSelectedRun(run);
     emit('runChanged');
     // close dropdown once clicked
