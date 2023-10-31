b635e4a1b9922166e8c004ef871f8d852ac11464
roger <roger.waldvogel@fhgr.ch>
2023-10-31 14:38:53 +0100

    Fixed scrolling issue.

--- components/document/documentSidebar.vue
+++ components/document/documentSidebar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="!loading" id="document-sidebar">
-    <div class="mt-5">
+  <div v-if="!loading" id="document-sidebar" class="flex h-full flex-col">
+    <div class="mt-5 grow-0">
       <OrbisButton :on-click="undoAnnotation" :disabled="isUndoDisabled">
         <OhVueIcon name="la-undo-alt-solid" />
       </OrbisButton>
@@ -8,60 +8,62 @@
         <OhVueIcon name="la-redo-alt-solid" />
       </OrbisButton>
     </div>
-    <div v-if="nestedSetRootNode">
-      <h2 class="p-2 text-4xl">Annotations</h2>
-      <table
-        aria-label="List of annotations"
-        class="table-auto border-spacing-1 text-gray-500 dark:text-gray-400"
-      >
-        <thead
-          class="bg-gray-50 text-left text-lg uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400"
+    <div v-if="nestedSetRootNode" class="flex min-h-0 grow flex-col">
+      <h2 class="grow-0 p-2 text-4xl">Annotations</h2>
+      <div class="min-h-0 flex-1 overflow-auto">
+        <table
+          aria-label="List of annotations"
+          class="border-spacing-1 text-gray-500 dark:text-gray-400"
         >
-          <tr>
-            <th class="p-2"></th>
-            <th class="p-2">start</th>
-            <th class="p-2">end</th>
-            <th class="p-2">surface</th>
-            <th class="p-2">type</th>
-            <th class="p-2"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="nestedSetNode in nestedSetRootNode.allAnnotationNodes()"
-            class="border-b bg-gray-50 hover:underline dark:border-gray-700 dark:bg-gray-800"
-            @mouseleave="emit('setHighlightNestedSetNode', null)"
-            @mouseout="emit('setHighlightNestedSetNode', null)"
-            @mouseover="emit('setHighlightNestedSetNode', nestedSetNode._id)"
+          <thead
+            class="bg-gray-50 text-left text-lg uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400"
           >
-            <td class="p-2">
-              <div
-                :style="{
-                  background:
-                    '#' +
-                    currentColorPalette.getHexadecimalColorValue(
-                      nestedSetNode.annotation_type.color_id,
-                    ),
-                }"
-                class="h-10 w-10 rounded-lg"
-              ></div>
-            </td>
-            <td class="p-2">{{ nestedSetNode.start_indices[0] }}</td>
-            <td class="p-2">{{ nestedSetNode.end_indices[0] }}</td>
-            <td class="p-2">{{ nestedSetNode.surface_forms[0] }}</td>
-            <td class="p-2">{{ nestedSetNode.annotation_type.name }}</td>
-            <td class="p-2">
-              <OrbisButton
-                :on-click="async () => deleteAnnotation(nestedSetNode)"
-                size="xs"
-              >
-                Delete
-                <OhVueIcon name="md-deleteforever-outlined" />
-              </OrbisButton>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            <tr>
+              <th class="p-2"></th>
+              <th class="p-2">start</th>
+              <th class="p-2">end</th>
+              <th class="p-2">surface</th>
+              <th class="p-2">type</th>
+              <th class="p-2"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="nestedSetNode in nestedSetRootNode.allAnnotationNodes()"
+              class="border-b bg-gray-50 hover:underline dark:border-gray-700 dark:bg-gray-800"
+              @mouseleave="emit('setHighlightNestedSetNode', null)"
+              @mouseout="emit('setHighlightNestedSetNode', null)"
+              @mouseover="emit('setHighlightNestedSetNode', nestedSetNode._id)"
+            >
+              <td class="p-2">
+                <div
+                  :style="{
+                    background:
+                      '#' +
+                      currentColorPalette.getHexadecimalColorValue(
+                        nestedSetNode.annotation_type.color_id,
+                      ),
+                  }"
+                  class="h-10 w-10 rounded-lg"
+                ></div>
+              </td>
+              <td class="p-2">{{ nestedSetNode.start_indices[0] }}</td>
+              <td class="p-2">{{ nestedSetNode.end_indices[0] }}</td>
+              <td class="p-2">{{ nestedSetNode.surface_forms[0] }}</td>
+              <td class="p-2">{{ nestedSetNode.annotation_type.name }}</td>
+              <td class="p-2">
+                <OrbisButton
+                  :on-click="async () => deleteAnnotation(nestedSetNode)"
+                  size="xs"
+                >
+                  Delete
+                  <OhVueIcon name="md-deleteforever-outlined" />
+                </OrbisButton>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -80,7 +82,7 @@ import { NestedSetNode } from "~/lib/model/nestedset/nestedSetNode";
 
 addIcons(LaUndoAltSolid, LaRedoAltSolid, MdDeleteforeverOutlined);
 
-const props = defineProps<{
+defineProps<{
   loading: boolean;
 }>();
 
--- layouts/defaultLayout.vue
+++ layouts/defaultLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex h-screen flex-col bg-base-100">
+  <div class="flex h-screen flex-col overflow-hidden bg-base-100">
     <header
       class="sticky top-0 z-10 col-span-full bg-base-200 px-4 py-2 text-gray-200"
     >
@@ -16,17 +16,17 @@
         </div>
       </div>
     </header>
-    <div class="flex flex-1">
+    <div class="flex h-full min-h-0 flex-1">
       <nav>
         <!-- Menu Entries -->
         <slot name="leftMenu">
           <LeftMenu />
         </slot>
       </nav>
-      <main class="grow overflow-auto p-4 pb-0">
+      <main class="h-full grow overflow-y-auto p-4 pb-0">
         <slot />
       </main>
-      <aside class="flex-shrink-0 overflow-auto">
+      <aside class="h-full">
         <!-- Sidebar -->
         <slot name="sidebar" />
       </aside>
@@ -35,10 +35,4 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { storeToRefs } from "pinia";
-import { useCorpusStore } from "~/stores/corpusStore";
-
-const corpusStore = useCorpusStore();
-const { corpus } = storeToRefs(corpusStore);
-</script>
+<script lang="ts" setup></script>
