058d6d6d3d37de49c80777177e1943a184385d38
Andreas Murk <andreas.murk@fhgr.ch>
2023-11-02 08:21:02 +0100

    feat(orbis): add rules for unused vars, camelCase and no use before define

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,8 @@ module.exports = {
   rules: {
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": "error",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/naming-convention": "error",
   },
 };
