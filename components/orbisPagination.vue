06118216ffb3ce78e51db7ebe5b601e3258e1a01
Andreas Murk <andreas.murk@fhgr.ch>
2023-11-02 11:41:46 +0100

    refactor(orbis): change to multi-word component

--- components/orbisPagination.vue
+++ components/orbisPagination.vue
@@ -29,11 +29,18 @@ const props = defineProps<{
   currentPage: number;
   totalPages: number;
 }>();
+
 const totalPages = toRef(props, "totalPages");
 const emit = defineEmits(["pageChanged"]);
 
 const isLastPage = computed(() => props.currentPage === props.totalPages);
 const isFirstPage = computed(() => props.currentPage === 1);
+
+function changePage(nextPage: number) {
+  if (nextPage > 0 && nextPage <= props.totalPages) {
+    emit("pageChanged", nextPage);
+  }
+}
 
 watch(totalPages, (newValue) => {
   // when nofPages changed go to last page
@@ -41,10 +48,4 @@ watch(totalPages, (newValue) => {
     changePage(newValue);
   }
 });
-
-function changePage(nextPage: number) {
-  if (nextPage > 0 && nextPage <= props.totalPages) {
-    emit("pageChanged", nextPage);
-  }
-}
 </script>
