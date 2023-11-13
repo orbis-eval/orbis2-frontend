ff68c772d58355611240a898a3f1e1fc989216a1
roger <roger.waldvogel@fhgr.ch>
2023-11-13 16:50:10 +0100

    default language should be english.

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,6 +1,6 @@
 export default defineI18nConfig(async () => ({
   legacy: false,
-  locale: "de",
+  locale: "en",
   messages: {
     en: await import("~/locales/en.json"),
     de: await import("~/locales/de.json"),
