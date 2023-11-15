032e7685f613daa5f5ef5a2681b763380cc3b96e
Andreas Murk <andreas.murk@fhgr.ch>
2023-11-15 17:38:19 +0100

    fix(orbis): skip _id
    remove transformToCamelCase

--- lib/utils/jsonTransformer.ts
+++ lib/utils/jsonTransformer.ts
@@ -29,58 +29,13 @@ export class JSONTransformer {
       if (Object.hasOwn(obj, key)) {
         const value = obj[key];
 
-        if (key === "id") {
+        if (key === "_id") {
           continue;
         }
 
         const transformedKey = key.replace(
           /[A-Z]/g,
           (match) => `_${match.toLowerCase()}`,
-        );
-
-        transformedObj[transformedKey] = processObjectValue(value);
-      }
-    }
-    return transformedObj;
-  }
-
-  /**
-   * Transforms the object(s) to a camel case object (e.g. surface_forms -> surfaceForms) to conform to the API
-   * @param obj
-   */
-  static transformToCamelCase(obj: Record<string, any>): Record<string, any> {
-    const transformedObj: Record<string, any> = {};
-
-    function processArray(arr: any[]): any[] {
-      return arr.map((item) => {
-        if (typeof item === "object" && item !== null) {
-          return JSONTransformer.transformToCamelCase(item);
-        }
-        return item;
-      });
-    }
-
-    function processObjectValue(value: any): any {
-      if (Array.isArray(value)) {
-        return processArray(value);
-      } else if (typeof value === "object" && value !== null) {
-        return JSONTransformer.transformToCamelCase(value);
-      } else {
-        return value;
-      }
-    }
-
-    for (const key in obj) {
-      if (Object.hasOwn(obj, key)) {
-        const value = obj[key];
-
-        if (key === "_id") {
-          transformedObj.id = value;
-          continue;
-        }
-
-        const transformedKey = key.replace(/_([a-z])/g, (_, p1) =>
-          p1.toUpperCase(),
         );
 
         transformedObj[transformedKey] = processObjectValue(value);
