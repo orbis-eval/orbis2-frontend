e2946560691d584f3886efe02cae637f887c1c0d
Andreas Murk <andreas.murk@fhgr.ch>
2023-11-23 10:15:44 +0100

    feat(orbis): add orbisApiService to colorPalettesStore

--- stores/colorPalettesStore.ts
+++ stores/colorPalettesStore.ts
@@ -2,12 +2,14 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import { OrbisApiService } from "~/lib/orbisApi/orbisApiService";
 import { ColorPalette } from "~/lib/model/colorpalette";
+import { ORBIS_BASE_URL } from "~/constants/orbisApi";
 
 export const useColorPalettesStore = defineStore("colorPalletes", () => {
+  const orbisApiService = new OrbisApiService(ORBIS_BASE_URL);
   const colorPalettes = ref([] as ColorPalette[]);
   const currentColorPalette = ref({} as ColorPalette);
 
-  async function loadColorPalettes(orbisApiService: OrbisApiService) {
+  async function loadColorPalettes() {
     try {
       colorPalettes.value = await orbisApiService.colorPalettes();
       currentColorPalette.value = colorPalettes.value[0];
