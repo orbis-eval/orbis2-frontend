2b643448bad1fe6ca177098fbffe054d6253acbe
Andreas Murk <andreas.murk@fhgr.ch>
2023-11-27 09:46:56 +0100

    feat(orbis): add simple messageToast component

---
+++ components/MessageToast.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <div class="toast toast-start">
+      <div v-if="toastSettings" :class="`alert alert-${toastSettings.type}`">
+        <span>{{ toastSettings.message }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MessageToastSettings } from "~/lib/types/MessageToastSettings";
+
+defineProps<{
+  toastSettings: MessageToastSettings;
+}>();
+</script>
