8cc1b6c8af800ba0845389aa4e08f5d2c4930f59
André Glatzl <andre.glatzl@fhgr.ch>
2024-01-30 14:19:40 +0100

    load gold standards

--- middleware/01.loadData.global.ts
+++ middleware/01.loadData.global.ts
@@ -6,7 +6,9 @@ export default defineNuxtRouteMiddleware((to, from) => {
   // check if route to has a runId param
   if (to.params.corpusId) {
     // if it does, set the runId in the store
-    runStore.loadGoldStandards(to.params.corpusId);
+    runStore.loadGoldStandards(to.params.corpusId).then(() => {
+      runStore.selectedGoldStandard = runStore.goldStandards[0];
+    });
   }
 
 });
