a740e0ad21e6c753537939dcb6fd40f552dd6f19
Andreas Murk <andreas.murk@fhgr.ch>
2024-02-29 15:28:29 +0100

    fix(orbis): fix test for now to run pipeline

--- tests/unit/stores/runStore.test.ts
+++ tests/unit/stores/runStore.test.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, vi, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { createPinia, setActivePinia } from "pinia";
 import { useRunStore } from "~/stores/runStore";
 import { Run } from "~/lib/model/run";
