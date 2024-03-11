3921cddbd3ec4b44d8708e9f7dd6330236cf7fba
Andreas Murk <andreas.murk@fhgr.ch>
2024-03-11 15:46:18 +0100

    feat(orbis): add getAlertClass to determine correct class
    When using enums you cannot directly use the enum value in the template

--- components/orbis/messageToast.vue
+++ components/orbis/messageToast.vue
@@ -3,7 +3,8 @@
     <div class="toast toast-start">
       <div
         v-if="toastSettings"
-        :class="`alert alert-${toastSettings.type.toString()} text-black`"
+        class="alert text-black"
+        :class="`alert ${getAlertClass(toastSettings.type)} text-black`"
       >
         <span>{{ toastSettings.message }}</span>
       </div>
@@ -13,6 +14,20 @@
 
 <script setup lang="ts">
 import { useMessageToast } from "~/composables/messageToast";
+import { MessageToastType } from "~/lib/types/MessageToastSettings";
 
 const { isVisible, toastSettings } = toRefs(useMessageToast());
+
+const getAlertClass = (type: MessageToastType) => {
+  switch (type) {
+    case MessageToastType.SUCCESS:
+      return "alert-success";
+    case MessageToastType.ERROR:
+      return "alert-error";
+    case MessageToastType.INFO:
+      return "alert-info";
+    default:
+      return ""; // Default class
+  }
+};
 </script>
