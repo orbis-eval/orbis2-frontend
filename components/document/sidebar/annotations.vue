a402c8ea26dfb9e85795f40308626cc399db836f
André Glatzl <andre.glatzl@fhgr.ch>
2024-04-11 14:17:16 +0200

    Cleaned up annotations; Added button to open document in gold standard, remove delete annotation button

--- components/document/sidebar/annotations.vue
+++ components/document/sidebar/annotations.vue
@@ -1,32 +1,32 @@
 <template>
   <div class="flex-1 overflow-auto p-4">
-    <div class="mb-2 text-lg font-bold">Annotations</div>
+    <div class="mb-2 text-lg font-bold">{{ $t("annotations") }}</div>
     <div v-if="isRun" role="tablist" class="tabs-boxed tabs">
+      <a
+          @click="activeAnnotationTab = 'all'"
+          role="tab"
+          class="tab"
+          :class="activeAnnotationTab == 'all' ? 'tab-active' : ''"
+      >All ({{ nestedSetRootNode.allAnnotationNodes().length }})</a
+      >
       <a
         @click="activeAnnotationTab = 'tp'"
         role="tab"
         class="tab"
         :class="activeAnnotationTab == 'tp' ? 'tab-active' : ''"
-        >TP</a
+        >TP ({{ currentDocument.scoring.tp.length }})</a
       >
       <a
         @click="activeAnnotationTab = 'fp'"
         role="tab"
         class="tab"
         :class="activeAnnotationTab == 'fp' ? 'tab-active' : ''"
-        >FP</a
-      >
-      <a
-        @click="activeAnnotationTab = 'fn'"
-        role="tab"
-        class="tab"
-        :class="activeAnnotationTab == 'fn' ? 'tab-active' : ''"
-        >FN</a
+        >FP ({{ currentDocument.scoring.fp.length }})</a
       >
     </div>
     <div v-if="nestedSetRootNode" class="space-y-2">
       <div
-        v-for="nestedSetNode in filterAnnotationNodes(
+        v-for="nestedSetNode in getNodes(
           nestedSetRootNode.allAnnotationNodes(),
         )"
         :key="nestedSetNode.identifier"
@@ -71,13 +71,6 @@
             </div>
             <div class="flex-grow"></div>
           </div>
-          <OrbisButton
-            :on-click="() => deleteAnnotation(nestedSetNode)"
-            size="xs"
-            transparent
-          >
-            <OhVueIcon name="md-deleteforever-outlined" />
-          </OrbisButton>
         </div>
       </div>
     </div>
@@ -85,50 +78,74 @@
 </template>
 
 <script lang="ts" setup>
-import {addIcons, OhVueIcon} from "oh-vue-icons";
-import {MdDeleteforeverOutlined} from "oh-vue-icons/icons";
-import {storeToRefs} from "pinia";
-import {useRunStore} from "~/stores/runStore";
-import {useDocumentStore} from "~/stores/documentStore";
-import {useAnnotationStore} from "~/stores/annotationStore";
-import {useColorPalettesStore} from "~/stores/colorPalettesStore";
-import {NestedSetNode} from "~/lib/model/nestedset/nestedSetNode";
+import { addIcons, OhVueIcon } from "oh-vue-icons";
+import { MdDeleteforeverOutlined } from "oh-vue-icons/icons";
+import { storeToRefs } from "pinia";
+import { useRunStore } from "~/stores/runStore";
+import { useDocumentStore } from "~/stores/documentStore";
+import { useAnnotationStore } from "~/stores/annotationStore";
+import { useColorPalettesStore } from "~/stores/colorPalettesStore";
+import { NestedSetNode } from "~/lib/model/nestedset/nestedSetNode";
 
 addIcons(MdDeleteforeverOutlined);
 
 const emit = defineEmits(["setHighlightNestedSetNode"]);
 
 const runStore = useRunStore();
-const {selectedRun} = storeToRefs(runStore);
+const { selectedRun } = storeToRefs(runStore);
 const documentStore = useDocumentStore();
-const {currentDocument} = storeToRefs(documentStore);
+const { currentDocument } = storeToRefs(documentStore);
 const annotationStore = useAnnotationStore();
-const {nestedSetRootNode, selectedAnnotation} = storeToRefs(annotationStore);
+const { nestedSetRootNode, selectedAnnotation } = storeToRefs(annotationStore);
 const colorPalettesStore = useColorPalettesStore();
-const {currentColorPalette} = storeToRefs(colorPalettesStore);
+const { currentColorPalette } = storeToRefs(colorPalettesStore);
 
 async function deleteAnnotation(nestedSetNode: NestedSetNode) {
   await annotationStore.deleteAnnotation(nestedSetNode);
 }
 
-const activeAnnotationTab = ref("tp");
+const activeAnnotationTab = ref("all");
 
 const isRun = computed(() => {
   return currentDocument.value.runId === selectedRun.value.identifier;
 });
 
-const filterAnnotationNodes = (nodes: NestedSetNode[]) => {
-  if (isRun.value) {
-    return nodes.filter((node) => {
-
-      if (activeAnnotationTab.value === "tp") {
-        return currentDocument.value.scoring.tp.find((tp) => tp.identifier === node.identifier);
-      } else if (activeAnnotationTab.value === "fp") {
-        return currentDocument.value.scoring.fp.find((fp) => fp.identifier === node.identifier);
-      } else if (activeAnnotationTab.value === "fn") {
-        return currentDocument.value.scoring.fn.find((fn) => fn.identifier === node.identifier);
+const filterAnnotationNodes = (nodes: NestedSetNode[], metricType: string) => {
+  return nodes.filter((node) => {
+    if (metricType === "tp") {
+      if (currentDocument.value.scoring.tp) {
+        return currentDocument.value.scoring.tp.find(
+          (tp) => tp.identifier === node.identifier,
+        );
       }
-    });
+      return false;
+    } else if (metricType === "fp") {
+      if (currentDocument.value.scoring.fp) {
+        return currentDocument.value.scoring.fp.find(
+          (fp) => fp.identifier === node.identifier,
+        );
+      }
+      return false;
+    } else if (metricType === "fn") {
+      if (currentDocument.value.scoring.fn) {
+        return currentDocument.value.scoring.fn.find(
+          (fn) => fn.identifier === node.identifier,
+        );
+      }
+      return false;
+    }
+  });
+};
+
+const getNodes = (nodes: NestedSetNode[]) => {
+  if (isRun.value) {
+    if (activeAnnotationTab.value === "tp") {
+      return filterAnnotationNodes(nodes, "tp");
+    } else if (activeAnnotationTab.value === "fp") {
+      return filterAnnotationNodes(nodes, "fp");
+    } else if (activeAnnotationTab.value === "all") {
+      return nodes;
+    }
   }
   return nodes;
 };
--- components/document/sidebar/index.vue
+++ components/document/sidebar/index.vue
@@ -5,6 +5,10 @@
 
       <!--<DocumentSidebarCommands></DocumentSidebarCommands>-->
 
+      <OrbisButton class="m-2" :on-click="() => openDocumentInGoldStandard()"
+        >{{ $t("document.sidebar.openInGoldStandard") }}
+      </OrbisButton>
+
       <DocumentSidebarAnnotations
         @set-highlight-nested-set-node="
           (nodeIDs) => emit('setHighlightNestedSetNode', nodeIDs)
@@ -16,12 +20,34 @@
 
 <script lang="ts" setup>
 import { storeToRefs } from "pinia";
+import { useRunStore } from "~/stores/runStore";
+import { useDocumentStore } from "~/stores/documentStore";
 import { useAnnotationStore } from "~/stores/annotationStore";
 
 const emit = defineEmits(["setHighlightNestedSetNode"]);
 
+const runStore = useRunStore();
+const { selectedRun } = storeToRefs(runStore);
+
+const documentStore = useDocumentStore();
+const { currentDocument } = storeToRefs(documentStore);
+
 const annotationStore = useAnnotationStore();
 const { selectedAnnotation } = storeToRefs(annotationStore);
+
+const router = useRouter();
+
+const openDocumentInGoldStandard = () => {
+  const routeData = router.resolve({
+    name: "corpora-corpusId-gold-standard-goldStandardId-documents-documentId",
+    params: {
+      corpusId: selectedRun.value.currentGoldStandard?.corpus.identifier,
+      goldStandardId: selectedRun.value.currentGoldStandard?.identifier,
+      documentId: currentDocument.value.identifier,
+    },
+  });
+  window.open(routeData.href, "_blank");
+};
 
 watch(selectedAnnotation, (newSelectedAnnotation) => {
   if (newSelectedAnnotation) {
