1293fc1e4af7c130be2e3aa92d4293d0731907e8
Andreas Murk <andreas.murk@fhgr.ch>
2024-04-22 18:13:23 +0200

    feat(orbis): revert back to original orbis theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["dark", "light"],
+    themes: [
+      {
+        dark: {
+          primary: "#C084FC",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#111827",
+          "base-100": "#374151",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+      "light",
+    ],
   },
   darkMode: "class",
 };
