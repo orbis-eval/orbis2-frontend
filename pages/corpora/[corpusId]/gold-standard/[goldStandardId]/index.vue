3f263ebde8abe47fbfb350738dd113cb5d507499
Andreas Murk <andreas.murk@fhgr.ch>
2024-06-04 12:09:23 +0200

    feat(frontend): add search for gold standard docs

--- pages/corpora/[corpusId]/gold-standard/[goldStandardId]/index.vue
+++ pages/corpora/[corpusId]/gold-standard/[goldStandardId]/index.vue
@@ -15,9 +15,17 @@
           }}
         </h1>
 
-        <h2 class="mb-5 text-2xl text-black dark:text-white">
-          {{ $t("documents") }}
-        </h2>
+        <div class="flex items-center">
+          <h2 class="mr-4 text-2xl">{{ $t("documents") }}</h2>
+          <input
+            type="text"
+            class="rounded-lg border-2 border-gray-600 p-2"
+            v-model="searchTerm"
+            :placeholder="$t('searchDocuments')"
+            @input="handleSearch"
+          />
+        </div>
+
         <div class="divider"></div>
         <table aria-label="List of documents in corpus" class="table table-sm">
           <thead class="text-left text-black dark:text-white">
@@ -73,6 +81,7 @@ import { useCorpusStore } from "~/stores/corpusStore";
 import { useDocumentStore } from "~/stores/documentStore";
 import { useGoldStandardStore } from "~/stores/goldStandardStore";
 import { useTitle } from "~/composables/title";
+import { debounce } from "lodash-es";
 
 addIcons(MdKeyboardarrowdown);
 
@@ -88,6 +97,7 @@ const goldStandardStore = useGoldStandardStore();
 const { selectedGoldStandard } = storeToRefs(goldStandardStore);
 
 const pageSize = ref(10);
+const searchTerm = ref("");
 
 const { documents, currentPage, totalPages } = storeToRefs(documentStore);
 
@@ -95,6 +105,30 @@ useTitle(
   `Gold Standard ${selectedGoldStandard.value.formattedCreatedAt}`,
   `${corpus.value.name} | Gold Standard ${selectedGoldStandard.value.formattedCreatedAt} | Documents`,
 );
+
+const handleSearch = debounce(async () => {
+  if (selectedGoldStandard.value.identifier) {
+    currentPage.value = 1; // Reset to first page when new search is performed
+    if (searchTerm.value.trim().length >= 3) {
+      await documentStore.loadDocuments(
+        selectedGoldStandard.value.identifier,
+        pageSize.value,
+        0,
+        searchTerm.value,
+      );
+      // update page count
+      countDocuments();
+    } else {
+      await documentStore.loadDocuments(
+        selectedGoldStandard.value.identifier,
+        pageSize.value,
+        0,
+        "",
+      );
+      countDocuments();
+    }
+  }
+}, 300);
 
 // called when another page is selected
 async function pageChanged(nextPage: number) {
@@ -115,9 +149,8 @@ async function pageChanged(nextPage: number) {
   }
 }
 
-async function countDocuments() {
+function countDocuments() {
   if (selectedGoldStandard.value.identifier) {
-    await documentStore.countDocuments(selectedGoldStandard.value.identifier);
     documentStore.totalPages = Math.ceil(
       documentStore.nrOfDocuments / pageSize.value,
     );
@@ -131,7 +164,7 @@ async function loadDocuments() {
 }
 
 onMounted(async () => {
-  await countDocuments();
   await loadDocuments();
+  countDocuments();
 });
 </script>
