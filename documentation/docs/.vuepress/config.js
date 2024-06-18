88ad61112a78cedc6e26914998cc38f58b53d67a
Andreas Murk <andreas.murk@fhgr.ch>
2024-06-18 14:44:37 +0200

    feat(vuepress): add src files

---
+++ documentation/docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+import { defaultTheme } from "@vuepress/theme-default";
+import { defineUserConfig } from "vuepress/cli";
+import { viteBundler } from "@vuepress/bundler-vite";
+
+export default defineUserConfig({
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "OrbisNG",
+      description: "Documentation Orbis NG",
+    },
+    "/de/": {
+      lang: "de-DE",
+      title: "OrbisNG",
+      description: "Dokumentation Orbis NG",
+    },
+  },
+
+  theme: defaultTheme({
+    // TODO: use real logo
+    logo: "assets/img/Orbis-Logo-Transparent.png",
+    locales: {
+      "/": {
+        navbar: [
+          { text: "Home", link: "/" },
+          { text: "Get Started", link: "/get-started" },
+          { text: "Quickstart", link: "/quickstart" },
+        ],
+      },
+      "/de/": {
+        navbar: [
+          { text: "Home", link: "/de/" },
+          { text: "Starten", link: "/de/start" },
+          { text: "Schnellstart", link: "/de/schnellstart" },
+        ],
+      },
+    },
+  }),
+
+  bundler: viteBundler(),
+});
